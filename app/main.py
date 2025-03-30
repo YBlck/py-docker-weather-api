4a05eb74cfee9db28244fe07b780b9d73c0998a5
Yura Chornyi <chorny.yura@gmail.com>
2025-03-30 05:59:10 +0300

    feat: implement get_weather func

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
+import os
+
+import requests
+
+URL = "http://api.weatherapi.com/v1/current.json"
+API_KEY = os.getenv("API_KEY")
+CITY = os.getenv("CITY", "Paris")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    print("Performing request to Weather API for city " + CITY + "...")
+    params = {"key": API_KEY, "q": CITY}
+    response = requests.get(URL, params=params)
+    weather_data = response.json()
+
+    if response.status_code == 200:
+        country = weather_data["location"]["country"]
+        city = weather_data["location"]["name"]
+        time = weather_data["location"]["localtime"]
+        temperature = weather_data["current"]["temp_c"]
+        condition = weather_data["current"]["condition"]["text"]
+        print(f"{city}/{country} {time} "
+              f"Weather: {temperature} Celsius, {condition}")
+    else:
+        print(weather_data["error"]["message"])
 
 
 if __name__ == "__main__":
